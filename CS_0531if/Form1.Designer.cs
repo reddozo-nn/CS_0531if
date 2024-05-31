f3a3a546da9afedaade1a1b04e3947df07772f33
Arakawa Ryu <104044024+ryu-ua@users.noreply.github.com>
2024-05-31 16:09:22 +0900

    演習8-4

--- CS_0531if/Form1.Designer.cs
+++ CS_0531if/Form1.Designer.cs
@@ -36,7 +36,7 @@
             // label1
             // 
             label1.AutoSize = true;
-            label1.Location = new Point(41, 403);
+            label1.Location = new Point(59, 411);
             label1.Name = "label1";
             label1.Size = new Size(38, 15);
             label1.TabIndex = 0;
--- CS_0531if/Form1.cs
+++ CS_0531if/Form1.cs
@@ -13,6 +13,22 @@ namespace CS_0531if
         {
             label1.Left += vx;
             label1.Top += vy;
+            if (label1.Left < 0)
+            {
+                vx = 15;
+            }
+            if (label1.Top < 0)
+            {
+                vy = 15;
+            }
+            if (label1.Top > 447)
+            {
+                vy = -15;
+            }
+            if (label1.Left > 763)
+            {
+                vy = -15;
+            }
         }
 
         private void label1_Click(object sender, EventArgs e)
